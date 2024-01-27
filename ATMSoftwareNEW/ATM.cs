dc35cfa9967dfefb95855c8e5784826fa7382c66
денис сл <pnas200@yandex.ru>
2024-01-28 02:17:47 +0300

    Добавил список операций, реализовал операцию выхода с деаутентификацией.

--- ATMSoftwareNEW/ATM.cs
+++ ATMSoftwareNEW/ATM.cs
@@ -18,17 +18,45 @@ namespace ATMSoftwareNEW
             {
                 Console.WriteLine("Добро пожаловать в систему банкомата «HAVBANK»");
 
-
                 string enteredNumberCard = GetUserInput("Введите номер вашей карты:");
                 string enteredPinCode = GetUserInput("Введите пин-код вашей карты:");
 
                 if (_bankSystem.Authenticate(enteredNumberCard, enteredPinCode))
                 {
-                    Console.WriteLine("Успешно!");
-                }
-                else
-                {
-                    Console.WriteLine("Нет");
+                    Console.WriteLine("Вход успешно совершён!");
+                    bool isEntered = true;
+
+                    while (isEntered)
+                    {
+                        Console.ReadKey();
+                        Console.Clear();
+
+                        Console.WriteLine("Выберите операцию:\n1 - Положить наличные\n2 - Снять наличные\n3 - Перевести деньги на другую карту\n4 - Изменить пин-код карты\n5 - Выход");
+                        string userInput = Console.ReadLine();
+
+                        switch (userInput)
+                        {
+                            case "1":
+                                break;
+
+                            case "2":
+                                break;
+
+                            case "3":
+                                break;
+
+                            case "4":
+                                break;
+
+                            case "5":
+                                isEntered = false;
+                                break;
+
+                            default:
+                                Console.WriteLine("Некорректный ввод!");
+                                break;
+                        }
+                    }
                 }
 
                 Console.ReadKey();
--- ATMSoftwareNEW/BankSystem.cs
+++ ATMSoftwareNEW/BankSystem.cs
@@ -35,7 +35,6 @@ namespace ATMSoftwareNEW
             }
 
             return false;
-
         }
     }
 }
